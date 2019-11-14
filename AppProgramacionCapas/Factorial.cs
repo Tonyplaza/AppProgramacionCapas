ac0d705f7ae1664d1451202ea6cb042a999a4038
Tonyplaza <plazatony3@gmail.com>
2019-11-14 09:49:12 -0500

    Agregando proyecyo de interfaz de usuario windoes

--- AppProgramacionCapas/Factorial.cs
+++ AppProgramacionCapas/Factorial.cs
@@ -8,8 +8,14 @@ namespace AppProgramacionCapas
 {
     public static class Factorial
     {
+        /// <summary>
+        /// Calcula el factorial de un numero
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
         public static long fact(int num)
         {
+            
             long f = 1;
             if (num == 0 || num == 1)
             {
@@ -20,6 +26,15 @@ namespace AppProgramacionCapas
                 f = f * i;
             }
             return f;
+        }
+        /// <summary>
+        /// Calcular el cuadrado de un numero
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static double cuadrado(double num)
+        {
+            return (num * num);
         }
     }
 }
